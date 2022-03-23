0f21c4e0e383f19441d1928e4387f89b0ae9fbb8
he2so0 <heesoo9913@gmail.com>
2022-03-23 20:59:50 +0900

    feat : 3/23, 메인페이지 슬라이드, 뉴스 수정

--- web/js/main.js
+++ web/js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 		$("#info01").show();
 		$("#info02").hide();
 		$("#info03").hide();
-		$("#info04").hide();
 	}
 })
 
@@ -21,27 +20,42 @@ function contentmenuClick(id){
 		$("#infolist01").attr('class','on');
 		$("#infolist02").attr('class','');
 		$("#infolist03").attr('class','');
-		$("#infolist04").attr('class','');
 
 		
 	}else if(id === 'infolist02'){
 		$("#infolist01").attr('class','');
 		$("#infolist02").attr('class','on');
 		$("#infolist03").attr('class','');
-		$("#infolist04").attr('class','');
 
 	}else if(id === 'infolist03'){
 		$("#infolist01").attr('class','');
 		$("#infolist02").attr('class','');
 		$("#infolist03").attr('class','on');
-		$("#infolist04").attr('class','');
 
-		$("#id").hide();
 	}else if(id === 'infolist04'){
 		$("#infolist01").attr('class','');
 		$("#infolist02").attr('class','');
 		$("#infolist03").attr('class','');
-		$("#infolist04").attr('class','on');
 
 	}
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }

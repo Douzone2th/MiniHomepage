d16bd08c2eb1eb05e836c4758d4ff83076b2f046
woohyeoung <dngud951108@gmail.com>
2022-03-23 18:53:44 +0900

    merge:3/23, merge to upstream main before

--- web/js/side_bar.js
+++ web/js/side_bar.js
@@ -1,8 +1,8 @@
 /* 왼쪽 사이드바 화살표 표시 */
 // active 화살표
 const active = document.querySelector(".active");
-const arrow = document.createElement('span');
-arrow.classList.add('material-icons');
+const arrow = document.createElement("span");
+arrow.classList.add("material-icons");
 arrow.innerText = "navigate_next";
 
 active.appendChild(arrow);

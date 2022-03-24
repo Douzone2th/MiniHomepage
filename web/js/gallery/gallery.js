36c496277c9823bbc491aa34c2a20360791fb00d
woohyeoung <dngud951108@gmail.com>
2022-03-24 10:13:44 +0900

    fix : merge

--- web/js/gallery/gallery.js
+++ web/js/gallery/gallery.js
@@ -1,11 +1,10 @@
 const body = document.querySelector("body");
-
 const mainBody = document.querySelector(".mainModal");
-
 //모달창 오픈
 function modalOpen(val) {
   $(mainBody).show();
   $(mainBody).children(".mainModalBody").attr("id", val);
+  location.href = "#modal_top";
 }
 //모달창 종료
 function modalClose() {
@@ -13,11 +12,9 @@ function modalClose() {
   body.style.overflow = "auto";
 }
 const subBody = document.querySelector(".subModal");
-
 function subOpen(id) {
   $(subBody).show();
   let val = $(".mainModalBody").attr("id");
-
   $(subBody).html(
     '<div class="subModalBody"><span class="close_modal" onclick="subClose()">&times;</span><img src="/images/' +
       id +
@@ -30,7 +27,6 @@ function subClose() {
   $(subBody).hide();
   body.style.overflow = "auto";
 }
-
 //팀과제 js
 //재빈
 const mainModal1 = document.getElementById("jae");

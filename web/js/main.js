04b73a00ef9ff83e534b0a5c25ddf14431db490b
he2so0 <heesoo9913@gmail.com>
2022-03-24 15:55:29 +0900

    merge:3/24, merge to upstream main before

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
 // main.js
 // main.js
-$(document).ready(function(){
-	if($("#infolist01").attr('class','on')){
+$(document).ready(function () {
+	if ($("#infolist01").attr('class', 'on')) {
 		$("#info01").show();
 		$("#info02").hide();
 		$("#info03").hide();
@@ -9,33 +9,33 @@ $(document).ready(function(){
 })
 
 
-function contentmenuClick(id){
-		
+function contentmenuClick(id) {
+
 	var id;
-	let info_id = "#infolist" + id.charAt(id.length-1);
-	$("#info0"+id.charAt(id.length-1)).toggle();
-	$(".min_cont").not("#info0"+id.charAt(id.length-1)).hide();
-	
-	if(id === 'infolist01'){
-		$("#infolist01").attr('class','on');
-		$("#infolist02").attr('class','');
-		$("#infolist03").attr('class','');
+	let info_id = "#infolist" + id.charAt(id.length - 1);
+	$("#info0" + id.charAt(id.length - 1)).toggle();
+	$(".min_cont").not("#info0" + id.charAt(id.length - 1)).hide();
 
-		
-	}else if(id === 'infolist02'){
-		$("#infolist01").attr('class','');
-		$("#infolist02").attr('class','on');
-		$("#infolist03").attr('class','');
+	if (id === 'infolist01') {
+		$("#infolist01").attr('class', 'on');
+		$("#infolist02").attr('class', '');
+		$("#infolist03").attr('class', '');
 
-	}else if(id === 'infolist03'){
-		$("#infolist01").attr('class','');
-		$("#infolist02").attr('class','');
-		$("#infolist03").attr('class','on');
 
-	}else if(id === 'infolist04'){
-		$("#infolist01").attr('class','');
-		$("#infolist02").attr('class','');
-		$("#infolist03").attr('class','');
+	} else if (id === 'infolist02') {
+		$("#infolist01").attr('class', '');
+		$("#infolist02").attr('class', 'on');
+		$("#infolist03").attr('class', '');
+
+	} else if (id === 'infolist03') {
+		$("#infolist01").attr('class', '');
+		$("#infolist02").attr('class', '');
+		$("#infolist03").attr('class', 'on');
+
+	} else if (id === 'infolist04') {
+		$("#infolist01").attr('class', '');
+		$("#infolist02").attr('class', '');
+		$("#infolist03").attr('class', '');
 
 	}
 }
@@ -44,18 +44,35 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {
+		slideIndex = 1
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+	setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+$('.dot').click((e) => {
+	let $target = $(e.target);
+	let uid = $target.attr('value');
+	var imageEl = document.querySelectorAll(".mySlides.fade");
+	$(imageEl[uid]).show().delay(3000);
+	$(imageEl).not(imageEl[uid]).hide().delay(3000); //클릭시 사진 더 보여줌
+})
+
+$('.dot.active').click((e) => {
+	let $target = $(e.target);
+	let uid = $target.attr('value');
+	var imageEl = document.querySelectorAll(".mySlides.fade");
+	$(imageEl[uid]).show().delay(3000);
+	$(imageEl).not(imageEl[uid]).hide().delay(3000); //클릭시 사진 더 보여줌
+})

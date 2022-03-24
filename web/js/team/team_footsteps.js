0cd2ab5e55263c9ae59868c671ef72f5c9dec022
Lee236 <sks1tp@naver.com>
2022-03-24 11:11:52 +0900

    merge:3/24, merge to upstream main before

--- web/js/team/team_footsteps.js
+++ web/js/team/team_footsteps.js
@@ -1,60 +1,28 @@
 String.prototype.format = function () {
-    var args = arguments;
-    return this.replace(/\{(\d+)\}/g, function (m, n) { return args[n]; });
-  };
+  var args = arguments;
+  return this.replace(/\{(\d+)\}/g, function (m, n) {
+    return args[n];
+  });
+};
 
-  // $(document).on("click", "#btn", function(){
-  //   var idx = $(this).index();
-  //   $('#timeline' + idx).show();
-    
-  //   jQuery(document).ready(function() { 
-  //     var lastYear;
-      
-  //     jQuery('#timeline{0}>li'.format(idx)).css("margin-bottom",0).each(function(){
-  //       var year=jQuery("input",this).val();
-  //       if (lastYear){
-  //           var px= (year - lastYear) * 5;
-  //           jQuery(this).css("margin-top",px);
-  //       }
-  //       lastYear = year;
-  //     });
-  //   });
-  // });
-
-
-
-
-$(document).ready(function(){
+$(document).ready(function () {
   $('#timeline0>li').hide()
   $('#timeline1>li').hide()
   $('#timeline2>li').hide()
   $('#timeline3>li').hide()
 })
 
-
-// $(document).on("click", "#btn", function(){
-//   var idx = $(this).index();
-//   $('#timeline' + idx + '>li').show();
-//   $("#timeline" + idx + ">li").toggle('slow')
-// });
-
-
-$(document).on("click", "#btn", function(){
+$(document).on("click", "#btn", function () {
   var idx = $(this).index();
   var lastYear;
   // $('#timeline' + idx).show();
-  jQuery("#timeline{0}>li".format(idx)).css("margin-bottom",0).each(function(){
-    var year=jQuery("input",this).val();
-    if (lastYear){
-        var px= (year - lastYear) * 5;
-        jQuery(this).css("margin-top",px);
+  jQuery("#timeline{0}>li".format(idx)).css("margin-bottom", 0).each(function () {
+    var year = jQuery("input", this).val();
+    if (lastYear) {
+      var px = (year - lastYear) * 5;
+      jQuery(this).css("margin-top", px);
     }
-    lastYear = year;    
+    lastYear = year;
   });
   $("#timeline{0}>li".format(idx)).toggle('slow');
 });
-
-
-
-  
-
--- web/js/team/test.js
+++
@@ -1,8 +0,0 @@
-var button = ["fill","pulse","close","up"];
-
-
-
-for( var i =0; i < button.length; i++){
-    console.log(button[i])
-
-}

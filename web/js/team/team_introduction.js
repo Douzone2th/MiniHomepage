43f764300a451dea0f15820da93d1bc4f26d6800
Lee236 <sks1tp@naver.com>
2022-03-25 09:15:38 +0900

    feat: 3/25, 최종

--- web/js/team/team_introduction.js
+++ web/js/team/team_introduction.js
@@ -1,32 +1,7 @@
-const show = document.querySelectorAll('.show-one')
-
 function biograph(e) {
-	var a = e.childNodes[3].childNodes[1].childNodes[0].data;
 	if (e.className == 'about__slot') {
-
 		e.className = 'about__slot bio';
-
-		if (a === "Jung JaeBin") {
-			$(show[0]).show();
-		} else if (a === "Lee Sangyoon") {
-			$(show[1]).show();
-		} else if (a === "Yoon Woohyeoung") {
-			$(show[2]).show();
-		} else if (a === "Kim Heesoo") {
-			$(show[3]).show();
-		}
 	} else {
-
 		e.className = 'about__slot';
-		if (a === "Jung JaeBin") {
-			$(show[0]).hide();
-		} else if (a === "Lee Sangyoon") {
-			$(show[1]).hide();
-		} else if (a === "Yoon Woohyeoung") {
-			$(show[2]).hide();
-		} else if (a === "Kim Heesoo") {
-			$(show[3]).hide();
-		}
 	}
-
 }
